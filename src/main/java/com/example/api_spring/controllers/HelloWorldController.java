ae96bc7e3dc81658d7ec302126e73b68d234637d
Fulldia <cam.lafourcade64@gmail.com>
2024-03-08 15:57:55 +0100

    ajout OPEN AI swagger

--- src/main/java/com/example/api_spring/controllers/HelloWorldController.java
+++ src/main/java/com/example/api_spring/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.api_spring.controllers;
 
 import com.example.api_spring.Salutations;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,7 @@ import java.util.Objects;
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public Salutations HelloWorld(@RequestParam String name, @RequestParam(defaultValue = "fr") String langue) {
+    public Salutations HelloWorld(@RequestParam String name, @Parameter(description = "coucou", example = "exemple") @RequestParam(defaultValue = "fr") String langue) {
         if (Objects.equals(name, "Clem")) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cam is not allowed");
         }
